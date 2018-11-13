6e998c9819170efc94e2b1c438836d0879936754
piopratama <piopratama2@gmail.com>
2018-11-13 21:25:58 +0800

    pio - change db and login level

--- process/loginProcess.php
+++ process/loginProcess.php
@@ -48,6 +48,23 @@ if(isset($_POST["username"]) && isset($_POST["password"]))
 			header("location:index.php");
 		}
 	}
+	elseif ($b=='2'){
+		
+		$sql = "SELECT id,username FROM tb_employee WHERE username = '$usernamed' AND `password`=MD5('".$password."');";
+		$result = $conn->query($sql);
+		if($result->num_rows > 0){
+			while($row = $result->fetch_assoc()){
+				$_SESSION["username"]=$usernamed;
+	        	$_SESSION["id_kasir"]=$row["id"];
+			}
+			$_SESSION['level_user']=0;
+			$sql1 = mysqli_query($conn, "update tb_employee set online_status='1' where username='$usernamed'");
+			header("location:../frontend/payment_menu.php");
+		}else{
+			$_SESSION["message"]="Login Failed";
+			header("location:index.php");
+		}
+	}
 	
 	$conn->close();
 
